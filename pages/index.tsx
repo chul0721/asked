0aaa1f60ebe7eb7186c19c39304a1c382d85528c
chul0721 <chul070721@naver.com>
2022-05-29 22:45:04 +0900

    done 🔥

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,134 @@
 import type { NextPage } from 'next'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 
 const Index: NextPage = () => {
-  return <></>
+  const router = useRouter()
+  const [data, setData] = useState([])
+  const [question, setQuestion] = useState('')
+  const [screen, setScreen] = useState(1)
+
+  const sendData = async () => {
+    if (question.length < 1) return alert('내용을 써')
+
+    await fetch('http://chul0721.iptime.org:4040/regist', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ content: question })
+    })
+
+    alert('질문 등록됐음')
+    router.reload()
+  }
+
+  const fetchData = async () => {
+    const res = await fetch('http://chul0721.iptime.org:4040/data')
+    const json = await res.json()
+
+    setData(json.data.reverse())
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return (
+    <>
+      <div className="flex flex-col">
+        <div className="grid justify-items-center pt-8 pb-4">
+          <div className="text-3xl font-semibold">섹시 병철이한테 무물ㅋ</div>
+          <div className="grid text-sm font-mono justify-items-center -space-y-1">
+            <span>심심해서 만든 서버 비용 낭비하는 짝퉁 에스크임</span>
+            <span>디자인은 구려도 양해 좀 ㅂㅌ</span>
+          </div>
+        </div>
+        <div className="grid justify-center space-y-3 mb-4">
+          <textarea
+            onChange={({ currentTarget: { value } }) => setQuestion(value)}
+            placeholder="무엇이든 물어보세요!"
+            className="border border-slate-100 p-3 rounded-2xl shadow-lg"
+            rows={5}
+            cols={35}
+          ></textarea>
+          <button
+            className="p-3 rounded-2xl shadow-lg bg-blue-200 font-semibold"
+            onClick={sendData}
+          >
+            질문하기
+          </button>
+        </div>
+      </div>
+      <div className="grid justify-center pt-20">
+        {screen === 1 && (
+          <>
+            <div className="flex">
+              <div
+                onClick={() => setScreen(1)}
+                className="cursor-pointer border-b px-10 md:px-20 py-2 bg-blue-200 rounded-t-lg shadow-md"
+              >
+                답변한 질문
+              </div>
+              <div
+                onClick={() => setScreen(2)}
+                className="cursor-pointer border-b px-10 md:px-20 py-2"
+              >
+                답변 안한 질문
+              </div>
+            </div>
+            <div>
+              {Object.entries(data).map(([_, question]: any) =>
+                question.isAnswered ? (
+                  <div key={question.id} className="bg-blue-50 rounded-xl p-4 my-10">
+                    <div className="text-2xl font-semibold">Q. {question.content}</div>
+                    <div className="text-xl pt-1">&gt; {question.answer}</div>
+                    <div className="font-mono text-sm text-slate-400 pt-3">
+                      {question.answeredAt.slice(0, 10)}
+                    </div>
+                  </div>
+                ) : (
+                  <></>
+                )
+              )}
+            </div>
+          </>
+        )}
+        {screen === 2 && (
+          <>
+            <div className="flex">
+              <div
+                onClick={() => setScreen(1)}
+                className="cursor-pointer border-b px-10 md:px-20 py-2"
+              >
+                답변한 질문
+              </div>
+              <div
+                onClick={() => setScreen(2)}
+                className="cursor-pointer border-b px-10 md:px-20 py-2 bg-blue-200 rounded-t-lg shadow-md"
+              >
+                답변 안한 질문
+              </div>
+            </div>
+            <div>
+              {Object.entries(data).map(([_, question]: any) =>
+                question.isAnswered ? (
+                  <></>
+                ) : (
+                  <div key={question.id} className="bg-blue-50 rounded-xl p-4 my-10">
+                    <div className="text-2xl font-semibold">Q. {question.content}</div>
+                    <div className="font-mono text-sm text-slate-400 pt-3">
+                      {question.answeredAt.slice(0, 10)}
+                    </div>
+                  </div>
+                )
+              )}
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  )
 }
 
 export default Index
